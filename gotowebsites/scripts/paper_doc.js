6bedeb39bb37bbdcedd728806b05242ef00cd7dd
DAMANPREET SINGH <damanstudyonly@gmail.com>
2026-09-16 15:54:46 -0400

    Update paper_doc.js

--- gotowebsites/scripts/paper_doc.js
+++ gotowebsites/scripts/paper_doc.js
@@ -14,7 +14,10 @@ if (doc_button_on) {
 }
 
 })
-
+doc_button.addEventListener("blur",()=>{
+    doc_section.classList.add('doc_deactive')
+    doc_button_on=true
+})
 
 const sheetLinks = [
   "https://docs.google.com/spreadsheets/d/1K6gkleEgJ8LLbI_63QvZ_unFiwMPxU4-c7NVu1Ou-U0/edit?usp=sharing",
